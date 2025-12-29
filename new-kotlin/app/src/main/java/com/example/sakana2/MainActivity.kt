78ef5816a0a0b25d0dc6304e4462ac62675bbf95
Inaba258 <inabagaj@gmail.com>
2025-12-29 15:02:47 +0900

    bc

--- new-kotlin/app/src/main/java/com/example/sakana2/MainActivity.kt
+++ new-kotlin/app/src/main/java/com/example/sakana2/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.sakana2.ui.theme.Sakana2Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +30,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Sakana2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    CompanyInfo(company = Company(
+                        "株式会社the pillows", "エントリー済"),
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -29,6 +39,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -38,10 +49,18 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    Sakana2Theme {
-        Greeting("Android")
+fun CompanyInfo(company: Company,modifier: Modifier = Modifier){
+    LazyColumn(modifier = modifier) {
+        item {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                 Text(text = company.comname)
+                 Text(text = company.comstatus) }
+            }
+        }
     }
-}
+
+
